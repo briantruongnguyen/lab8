d4065df422fab16aa0ce23d57fb7bb5207a37730
Brian Truong Nguyen <btn023@ucsd.edu>
2017-03-01 23:04:29 -0800

    rip

--- public/js/googlemaps.js
+++ public/js/googlemaps.js
@@ -1,6 +1,6 @@
 function initAutocomplete() {
   // Create center marker at UCSD
-  var ucsd_ltlng = {lat:32.880, lng:-117.236};
+  var ucsd_ltlng = {lat:32.8850016, lng:-117.2412938};
 
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
