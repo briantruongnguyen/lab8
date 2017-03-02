c330f8aa71b7a3a118047b7a5e0bc422a6f15913
Brian Truong Nguyen <btn023@ucsd.edu>
2017-03-02 02:27:19 -0800

    lol

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,6 +1,6 @@
 function initCamera() {
   //Uncomment and fill in the correct selectors below.
-  capture($("#camera-video"), $("#camera-canvas"), $("#camera-button"));
+  capture($("#camera-video"), $("#camera-canvas"), $("#camera-buttongit add"));
 }
 
 function capture(video, canvas, snapshotButton) {
